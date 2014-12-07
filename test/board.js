c7e0ec23fe7cf5311b09c1e59799fb5842975f10
Chris LaRose <cjlarose@gmail.com>
2014-12-07 15:24:25 -0800

    Add game tests

--- test/board.js
+++ test/board.js
@@ -2,141 +2,143 @@ var should = require('chai').should();
 var expect = require('chai').expect;
 var Board = require('../index.js').Board;
 
-describe('#createBoard', function() {
-  it('creates a board of size 9', function() {
-    var board = Board.createBoard(9);
-    board.size.should.equal(9);
+describe("Board", function() {
+  describe('#createBoard', function() {
+    it('creates a board of size 9', function() {
+      var board = Board.createBoard(9);
+      board.size.should.equal(9);
+    });
+
+    it('creates a board of size 13', function() {
+      var board = Board.createBoard(13);
+      board.size.should.equal(13);
+    });
+
+    it('creates a board of size 19', function() {
+      var board = Board.createBoard(19);
+      board.size.should.equal(19);
+    });
+
+    it('should start off empty', function() {
+      var board = Board.createBoard(9);
+      var i, j;
+      for (i = 0; i < 9; i++)
+        for (j = 0; j < 9; j++)
+          board.getStone(i, j).should.equal(Board.EMPTY);
+    });
   });
 
-  it('creates a board of size 13', function() {
-    var board = Board.createBoard(13);
-    board.size.should.equal(13);
+  describe('#inBounds', function() {
+    it('should accept inner bounds', function() {
+      var board = Board.createBoard(9);
+      var i, j;
+      for (i = 0; i < 9; i++)
+        for (j = 0; j < 9; j++)
+          board.inBounds(i, j).should.equal(true);
+    });
+
+    it('should reject negative coords', function() {
+      Board.createBoard(9).inBounds(-1, 0).should.equal(false);
+      Board.createBoard(9).inBounds(0, -1).should.equal(false);
+      Board.createBoard(9).inBounds(-1, -1).should.equal(false);
+    });
+
+
+    it('should reject high coords', function() {
+      Board.createBoard(9).inBounds(9, 0).should.equal(false);
+      Board.createBoard(9).inBounds(0, 9).should.equal(false);
+      Board.createBoard(9).inBounds(9, 9).should.equal(false);
+    });
   });
 
-  it('creates a board of size 19', function() {
-    var board = Board.createBoard(19);
-    board.size.should.equal(19);
-  });
+  describe('#play', function() {
+    it('should reject out of bounds coords', function() {
+      var fn = function() {
+        return Board.createBoard(9).play(Board.BLACK, -1, -1);
+      };
+      expect(fn).to.throw("Intersection out of bounds");
+    });
 
-  it('should start off empty', function() {
-    var board = Board.createBoard(9);
-    var i, j;
-    for (i = 0; i < 9; i++)
-      for (j = 0; j < 9; j++)
-        board.getStone(i, j).should.equal(Board.EMPTY);
-  });
-});
+    it('should reject occupied intersections', function() {
+      var fn = function() {
+        Board.createBoard(9)
+          .play(Board.BLACK, 0, 0)
+          .play(Board.WHITE, 0, 0);
+      };
+      expect(fn).to.throw("Intersection occupied by existing stone");
+    });
 
-describe('#inBounds', function() {
-  it('should accept inner bounds', function() {
-    var board = Board.createBoard(9);
-    var i, j;
-    for (i = 0; i < 9; i++)
-      for (j = 0; j < 9; j++)
-        board.inBounds(i, j).should.equal(true);
-  });
-
-  it('should reject negative coords', function() {
-    Board.createBoard(9).inBounds(-1, 0).should.equal(false);
-    Board.createBoard(9).inBounds(0, -1).should.equal(false);
-    Board.createBoard(9).inBounds(-1, -1).should.equal(false);
-  });
-
-
-  it('should reject high coords', function() {
-    Board.createBoard(9).inBounds(9, 0).should.equal(false);
-    Board.createBoard(9).inBounds(0, 9).should.equal(false);
-    Board.createBoard(9).inBounds(9, 9).should.equal(false);
-  });
-});
-
-describe('#play', function() {
-  it('should reject out of bounds coords', function() {
-    var fn = function() {
-      return Board.createBoard(9).play(Board.BLACK, -1, -1);
-    };
-    expect(fn).to.throw("Intersection out of bounds");
-  });
-
-  it('should reject occupied intersections', function() {
-    var fn = function() {
-      Board.createBoard(9)
+    it('should set the correct stone color', function() {
+      Board.createBoard(4)
         .play(Board.BLACK, 0, 0)
-        .play(Board.WHITE, 0, 0);
-    };
-    expect(fn).to.throw("Intersection occupied by existing stone");
-  });
+        .toString().should.equal("x...\n....\n....\n....");
 
-  it('should set the correct stone color', function() {
-    Board.createBoard(4)
-      .play(Board.BLACK, 0, 0)
-      .toString().should.equal("x...\n....\n....\n....");
+      Board.createBoard(4)
+        .play(Board.WHITE, 3, 2)
+        .toString().should.equal("....\n....\n....\n..o.");
+    });
 
-    Board.createBoard(4)
-      .play(Board.WHITE, 3, 2)
-      .toString().should.equal("....\n....\n....\n..o.");
-  });
+    it('should capture stones in the corner', function() {
+      var board = Board.createBoard(4)
+                    .play(Board.BLACK, 0, 0)
+                    .play(Board.BLACK, 0, 1)
+                    .play(Board.BLACK, 1, 0);
 
-  it('should capture stones in the corner', function() {
-    var board = Board.createBoard(4)
-                  .play(Board.BLACK, 0, 0)
-                  .play(Board.BLACK, 0, 1)
-                  .play(Board.BLACK, 1, 0);
+      board.toString().should.equal("xx..\nx...\n....\n....");
 
-    board.toString().should.equal("xx..\nx...\n....\n....");
+      board
+        .play(Board.WHITE, 0, 2)
+        .play(Board.WHITE, 1, 1);
 
-    board
-      .play(Board.WHITE, 0, 2)
-      .play(Board.WHITE, 1, 1);
+      board.toString().should.equal("xxo.\nxo..\n....\n....");
 
-    board.toString().should.equal("xxo.\nxo..\n....\n....");
+      board
+        .play(Board.WHITE, 2, 0);
 
-    board
-      .play(Board.WHITE, 2, 0);
+      board.toString().should.equal("..o.\n.o..\no...\n....");
+    });
 
-    board.toString().should.equal("..o.\n.o..\no...\n....");
-  });
+    it('should capture stones on the side', function() {
+      var board = Board.createBoard(4)
+                    .play(Board.BLACK, 1, 3)
+                    .play(Board.BLACK, 1, 2);
 
-  it('should capture stones on the side', function() {
-    var board = Board.createBoard(4)
-                  .play(Board.BLACK, 1, 3)
-                  .play(Board.BLACK, 1, 2);
+      board.toString().should.equal("....\n..xx\n....\n....");
 
-    board.toString().should.equal("....\n..xx\n....\n....");
+      board
+        .play(Board.WHITE, 1, 1)
+        .play(Board.WHITE, 0, 3)
+        .play(Board.WHITE, 0, 2)
+        .play(Board.WHITE, 2, 3);
 
-    board
-      .play(Board.WHITE, 1, 1)
-      .play(Board.WHITE, 0, 3)
-      .play(Board.WHITE, 0, 2)
-      .play(Board.WHITE, 2, 3);
+      board.toString().should.equal("..oo\n.oxx\n...o\n....");
 
-    board.toString().should.equal("..oo\n.oxx\n...o\n....");
+      board
+        .play(Board.WHITE, 2, 2);
 
-    board
-      .play(Board.WHITE, 2, 2);
+      board.toString().should.equal("..oo\n.o..\n..oo\n....");
+    });
 
-    board.toString().should.equal("..oo\n.o..\n..oo\n....");
-  });
+    it('should capture stones in the middle', function() {
+      var board = Board.createBoard(4)
+                    .play(Board.BLACK, 1, 1)
+                    .play(Board.BLACK, 1, 2);
 
-  it('should capture stones in the middle', function() {
-    var board = Board.createBoard(4)
-                  .play(Board.BLACK, 1, 1)
-                  .play(Board.BLACK, 1, 2);
+      board.toString().should.equal("....\n.xx.\n....\n....");
 
-    board.toString().should.equal("....\n.xx.\n....\n....");
+      board
+        .play(Board.WHITE, 0, 2)
+        .play(Board.WHITE, 1, 0)
+        .play(Board.WHITE, 1, 3)
+        .play(Board.WHITE, 2, 1)
+        .play(Board.WHITE, 2, 2);
 
-    board
-      .play(Board.WHITE, 0, 2)
-      .play(Board.WHITE, 1, 0)
-      .play(Board.WHITE, 1, 3)
-      .play(Board.WHITE, 2, 1)
-      .play(Board.WHITE, 2, 2);
+      board.toString().should.equal("..o.\noxxo\n.oo.\n....");
 
-    board.toString().should.equal("..o.\noxxo\n.oo.\n....");
+      board
+        .play(Board.WHITE, 0, 1);
 
-    board
-      .play(Board.WHITE, 0, 1);
-
-    board.toString().should.equal(".oo.\no..o\n.oo.\n....");
+      board.toString().should.equal(".oo.\no..o\n.oo.\n....");
+    });
   });
 });
---
+++ test/game.js
@@ -0,0 +1,57 @@
+var should = require('chai').should();
+var expect = require('chai').expect;
+var Game = require('../index.js').Game;
+var Board = require('../index.js').Board;
+
+describe("Game", function() {
+  describe('#createGame', function() {
+    it('started with black player', function() {
+      var game = Game.createGame(9);
+      game.getCurrentPlayer().should.equal(Board.BLACK);
+      game.isOver().should.equal(false);
+    });
+  });
+
+  describe('#pass', function() {
+    it('ends the game with two consectutive passes', function() {
+      var game = Game.createGame(9);
+      game.pass(Board.BLACK);
+      game.getCurrentPlayer().should.equal(Board.WHITE);
+      game.pass(Board.WHITE);
+      game.isOver().should.equal(true);
+
+      var fn = function() {game.pass(Board.BLACK);};
+      expect(fn).to.throw("Game is already over");
+    });
+
+    it('forbids play of same player twice', function() {
+      var game = Game.createGame(9);
+      game.pass(Board.BLACK);
+      game.getCurrentPlayer().should.equal(Board.WHITE);
+
+      var fn = function() {game.pass(Board.BLACK);};
+      expect(fn).to.throw("Not player's turn");
+    });
+  });
+
+  describe("#play", function() {
+    it('forbids play on completed game', function() {
+      var game = Game.createGame(9);
+      game.pass(Board.BLACK);
+      game.pass(Board.WHITE);
+      game.isOver().should.equal(true);
+
+      var fn = function() {game.play(Board.BLACK);};
+      expect(fn).to.throw("Game is already over");
+    });
+
+    it('forbids play of same player twice', function() {
+      var game = Game.createGame(9);
+      game.play(Board.BLACK, 0, 0);
+      game.getCurrentPlayer().should.equal(Board.WHITE);
+
+      var fn = function() {game.play(Board.BLACK, 0, 0);};
+      expect(fn).to.throw("Not player's turn");
+    });
+  });
+});
