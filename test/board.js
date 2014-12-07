db1eb8f2462b99e59ff8d9e3454e7687ca8cec2d
Chris LaRose <cjlarose@gmail.com>
2014-12-07 14:12:54 -0800

    Test board boundaries

--- test/board.js
+++ test/board.js
@@ -17,3 +17,28 @@ describe('#createBoard', function() {
     board.size.should.equal(19);
   });
 });
+
+describe('#inBounds', function() {
+  it('should accept inner bounds', function() {
+    var board = Board.createBoard(9);
+    var i, j;
+    for (i = 0; i < 9; i++) {
+      for (j = 0; j < 9; j++) {
+        board.inBounds(i, j).should.equal(true);
+      }
+    }
+  });
+
+  it('should reject negative coords', function() {
+    Board.createBoard(9).inBounds(-1, 0).should.equal(false);
+    Board.createBoard(9).inBounds(0, -1).should.equal(false);
+    Board.createBoard(9).inBounds(-1, -1).should.equal(false);
+  });
+
+
+  it('should reject high coords', function() {
+    Board.createBoard(9).inBounds(9, 0).should.equal(false);
+    Board.createBoard(9).inBounds(0, 9).should.equal(false);
+    Board.createBoard(9).inBounds(9, 9).should.equal(false);
+  });
+});
