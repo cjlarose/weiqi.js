2935fd192ea59aa4991a2bd8cd69ced299a31937
Chris LaRose <cjlarose@gmail.com>
2014-12-07 14:38:47 -0800

    Add some tests for Board.play

--- test/board.js
+++ test/board.js
@@ -1,4 +1,5 @@
 var should = require('chai').should();
+var expect = require('chai').expect;
 var Board = require('../index.js').Board;
 
 describe('#createBoard', function() {
@@ -40,5 +41,33 @@ describe('#inBounds', function() {
     Board.createBoard(9).inBounds(9, 0).should.equal(false);
     Board.createBoard(9).inBounds(0, 9).should.equal(false);
     Board.createBoard(9).inBounds(9, 9).should.equal(false);
+  });
+});
+
+describe('#play', function() {
+  it('should reject out of bounds coords', function() {
+    var fn = function() {
+      return Board.createBoard(9).play(Board.BLACK, -1, -1);
+    };
+    expect(fn).to.throw("Intersection out of bounds");
+  });
+
+  it('should reject occupied intersections', function() {
+    var fn = function() {
+      Board.createBoard(9)
+        .play(Board.BLACK, 0, 0)
+        .play(Board.WHITE, 0, 0);
+    };
+    expect(fn).to.throw("Intersection occupied by existing stone");
+  });
+
+  it('should set the correct stone color', function() {
+    Board.createBoard(9)
+      .play(Board.BLACK, 0, 0)
+      .getStone(0, 0).should.equal(Board.BLACK);
+
+    Board.createBoard(9)
+      .play(Board.WHITE, 4, 2)
+      .getStone(4, 2).should.equal(Board.WHITE);
   });
 });
