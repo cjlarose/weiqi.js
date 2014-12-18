b88b0f4a5b5f98763144d2b00708f43657cefb63
Chris LaRose <cjlarose@gmail.com>
2014-12-17 21:39:15 -0800

    Add positional superko detection

--- test/game.js
+++ test/game.js
@@ -53,5 +53,19 @@ describe("Game", function() {
       var fn = function() {game.play(Weiqi.BLACK, [0, 0]);};
       expect(fn).to.throw("Not player's turn");
     });
+
+    it('forbids simple ko', function() {
+      var game = Game.createGame(4)
+                   .play(Weiqi.BLACK, [0, 1])
+                   .play(Weiqi.WHITE, [0, 2])
+                   .play(Weiqi.BLACK, [1, 2])
+                   .play(Weiqi.WHITE, [1, 3])
+                   .play(Weiqi.BLACK, [2, 1])
+                   .play(Weiqi.WHITE, [2, 2])
+                   .play(Weiqi.BLACK, [1, 0])
+                   .play(Weiqi.WHITE, [1, 1]);
+      var fn = function() {game.play(Weiqi.BLACK, [1, 2]);};
+      expect(fn).to.throw("Violation of Ko");
+    });
   });
 });
