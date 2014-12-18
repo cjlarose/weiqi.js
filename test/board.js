9e73bd94f84d47d1705ccac0a0d2c224591e54d4
Chris LaRose <cjlarose@gmail.com>
2014-12-17 20:48:07 -0800

    Remove Board.inBounds from public interface

--- test/board.js
+++ test/board.js
@@ -29,35 +29,24 @@ describe("Board", function() {
     });
   });
 
-  describe('#inBounds', function() {
-    it('should accept inner bounds', function() {
-      var board = Board.createBoard(9);
-      var i, j;
-      for (i = 0; i < 9; i++)
-        for (j = 0; j < 9; j++)
-          board.inBounds([i, j]).should.equal(true);
-    });
-
-    it('should reject negative coords', function() {
-      Board.createBoard(9).inBounds(-1, 0).should.equal(false);
-      Board.createBoard(9).inBounds(0, -1).should.equal(false);
-      Board.createBoard(9).inBounds(-1, -1).should.equal(false);
-    });
-
-
-    it('should reject high coords', function() {
-      Board.createBoard(9).inBounds(9, 0).should.equal(false);
-      Board.createBoard(9).inBounds(0, 9).should.equal(false);
-      Board.createBoard(9).inBounds(9, 9).should.equal(false);
-    });
-  });
-
   describe('#play', function() {
+    it('should allow inner coords', function() {
+      var i, j, board;
+      for (i = 0; i < 9; i++)
+        for (j = 0; j < 9; j++) {
+          board = Board.createBoard(9).play(Weiqi.BLACK, [i, j]);
+          board.getStone([i, j]).should.equal(Weiqi.BLACK);
+        }
+    });
+
     it('should reject out of bounds coords', function() {
-      var fn = function() {
-        return Board.createBoard(9).play(Weiqi.BLACK, [-1, -1]);
-      };
-      expect(fn).to.throw("Intersection out of bounds");
+      var positions = [[-1, 0], [0, -1], [-1, -1], [9, 0], [0, 9], [9, 9]];
+      positions.forEach(function(position) {
+        var fn = function() {
+          return Board.createBoard(9).play(Weiqi.BLACK, positions);
+        };
+        expect(fn).to.throw("Intersection out of bounds");
+      });
     });
 
     it('should reject occupied intersections', function() {
