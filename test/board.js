4697696b75c69af42a29d0a6c2a876810a8db3b0
Chris LaRose <cjlarose@gmail.com>
2015-04-30 19:27:39 -0700

    cjl - Remove chai from board tests

--- test/board.js
+++ test/board.js
@@ -1,61 +1,57 @@
-var should = require('chai').should();
-var expect = require('chai').expect;
+var assert = require('assert');
+var Immutable = require('immutable');
 var Weiqi = require('../dist/index.js');
 
-// shorthand for testing whole board
-Object.prototype.looksLike = function(rows) {
-  var expected = [];
-  rows.forEach(function(str) {
-    expected.push(str.split(''));
+const assertEqualBoards = function(expectedRows, board) {
+  var rows = expectedRows.map(function(str) {
+    return Immutable.List(str.split(''));
   });
-  return this.toArray().should.deep.equal(expected);
-};
+  var expectedGrid = Immutable.List(rows);
+  assert(expectedGrid, board.getIntersections());
+}
 
 describe("Board", function() {
   describe('#createBoard', function() {
     it('creates a board of size 9', function() {
       var board = Weiqi.createBoard(9);
-      board.getSize().should.equal(9);
+      assert.equal(9, board.getSize());
     });
 
     it('creates a board of size 13', function() {
       var board = Weiqi.createBoard(13);
-      board.getSize().should.equal(13);
+      assert.equal(13, board.getSize());
     });
 
     it('creates a board of size 19', function() {
       var board = Weiqi.createBoard(19);
-      board.getSize().should.equal(19);
+      assert.equal(19, board.getSize());
     });
 
     it('should start off empty', function() {
       var board = Weiqi.createBoard(9);
-      var i, j;
-      for (i = 0; i < 9; i++)
-        for (j = 0; j < 9; j++)
-          board.getStone([i, j]).should.equal(Weiqi.EMPTY);
+      for (var i = 0; i < 9; i++)
+        for (var j = 0; j < 9; j++)
+          assert.equal(Weiqi.EMPTY, board.getStone([i, j]));
     });
 
     it('requires a size', function() {
       var fn = function() { Weiqi.createBoard(); };
-      expect(fn).to.throw("Size must be an integer greater than zero");
+      assert.throws(fn, /^Size must be an integer greater than zero$/);
     });
 
     it('rejects negative size', function() {
       var fn = function() { Weiqi.createBoard(-9); };
-      expect(fn).to.throw("Size must be an integer greater than zero");
+      assert.throws(fn, /^Size must be an integer greater than zero$/);
     });
   });
 
   describe('#play', function() {
     it('should allow inner coords', function() {
-      var i, j, board;
       var board = Weiqi.createBoard(9);
-      for (i = 0; i < 9; i++)
-        for (j = 0; j < 9; j++) {
-          board
-            .play(Weiqi.BLACK, [i, j])
-           .getStone([i, j]).should.equal(Weiqi.BLACK);
+      for (var i = 0; i < 9; i++)
+        for (var j = 0; j < 9; j++) {
+          var stone = board.play(Weiqi.BLACK, [i, j]).getStone([i, j]);
+          assert.equal(Weiqi.BLACK, stone);
         }
     });
 
@@ -65,7 +61,7 @@ describe("Board", function() {
         var fn = function() {
           return Weiqi.createBoard(9).play(Weiqi.BLACK, positions);
         };
-        expect(fn).to.throw("Intersection out of bounds");
+        assert.throws(fn, /^Intersection out of bounds$/);
       });
     });
 
@@ -75,17 +71,15 @@ describe("Board", function() {
           .play(Weiqi.BLACK, [0, 0])
           .play(Weiqi.WHITE, [0, 0]);
       };
-      expect(fn).to.throw("Intersection occupied by existing stone");
+      assert.throws(fn, /^Intersection occupied by existing stone$/);
     });
 
     it('should set the correct stone color', function() {
-      Weiqi.createBoard(4)
-        .play(Weiqi.BLACK, [0, 0])
-        .looksLike(['x...', '....', '....', '....']);
+      assertEqualBoards(['x...', '....', '....', '....'],
+                        Weiqi.createBoard(4).play(Weiqi.BLACK, [0, 0]));
 
-      Weiqi.createBoard(4)
-        .play(Weiqi.WHITE, [3, 2])
-        .looksLike(['....', '....', '....', '..o.']);
+      assertEqualBoards(['....', '....', '....', '..o.'],
+                        Weiqi.createBoard(4).play(Weiqi.WHITE, [3, 2]));
     });
 
     it('should capture stones in the corner', function() {
@@ -94,16 +88,16 @@ describe("Board", function() {
                     .play(Weiqi.BLACK, [0, 1])
                     .play(Weiqi.BLACK, [1, 0]);
 
-      board.looksLike(['xx..', 'x...', '....', '....']);
+      assertEqualBoards(['xx..', 'x...', '....', '....'], board);
 
       board = board
         .play(Weiqi.WHITE, [0, 2])
         .play(Weiqi.WHITE, [1, 1]);
 
-      board.looksLike(['xxo.', 'xo..', '....', '....']);
+      assertEqualBoards(['xxo.', 'xo..', '....', '....'], board);
 
       board = board.play(Weiqi.WHITE, [2, 0]);
-      board.looksLike(['..o.', '.o..', 'o...', '....']);
+      assertEqualBoards(['..o.', '.o..', 'o...', '....'], board);
     });
 
     it('should capture stones on the side', function() {
@@ -111,7 +105,7 @@ describe("Board", function() {
                     .play(Weiqi.BLACK, [1, 3])
                     .play(Weiqi.BLACK, [1, 2]);
 
-      board.looksLike(['....', '..xx', '....', '....']);
+      assertEqualBoards(['....', '..xx', '....', '....'], board);
 
       board = board
         .play(Weiqi.WHITE, [1, 1])
@@ -119,9 +113,9 @@ describe("Board", function() {
         .play(Weiqi.WHITE, [0, 2])
         .play(Weiqi.WHITE, [2, 3]);
 
-      board.looksLike(['..oo', '.oxx', '...o', '....']);
+      assertEqualBoards(['..oo', '.oxx', '...o', '....'], board);
       board = board.play(Weiqi.WHITE, [2, 2]);
-      board.looksLike(['..oo', '.o..', '..oo', '....']);
+      assertEqualBoards(['..oo', '.o..', '..oo', '....'], board);
     });
 
     it('should capture stones in the middle', function() {
@@ -129,7 +123,7 @@ describe("Board", function() {
                     .play(Weiqi.BLACK, [1, 1])
                     .play(Weiqi.BLACK, [1, 2]);
 
-      board.looksLike(['....', '.xx.', '....', '....']);
+      assertEqualBoards(['....', '.xx.', '....', '....'], board);
 
       board = board
         .play(Weiqi.WHITE, [0, 2])
@@ -138,9 +132,9 @@ describe("Board", function() {
         .play(Weiqi.WHITE, [2, 1])
         .play(Weiqi.WHITE, [2, 2]);
 
-      board.looksLike(['..o.', 'oxxo', '.oo.', '....']);
+      assertEqualBoards(['..o.', 'oxxo', '.oo.', '....'], board);
       board = board.play(Weiqi.WHITE, [0, 1]);
-      board.looksLike(['.oo.', 'o..o', '.oo.', '....']);
+      assertEqualBoards(['.oo.', 'o..o', '.oo.', '....'], board);
     });
 
     it('should allow suicide of one stone', function() {
@@ -150,9 +144,9 @@ describe("Board", function() {
                     .play(Weiqi.BLACK, [2, 1])
                     .play(Weiqi.BLACK, [1, 0]);
 
-      board.looksLike(['.x..', 'x.x.', '.x..', '....']);
+      assertEqualBoards(['.x..', 'x.x.', '.x..', '....'], board);
       board = board.play(Weiqi.WHITE, [1, 1]);
-      board.looksLike(['.x..', 'x.x.', '.x..', '....']);
+      assertEqualBoards(['.x..', 'x.x.', '.x..', '....'], board);
     });
 
     it('should allow suicide of many stones', function() {
@@ -164,11 +158,11 @@ describe("Board", function() {
                     .play(Weiqi.BLACK, [2, 1])
                     .play(Weiqi.BLACK, [1, 0]);
 
-      board.looksLike(['.xx.', 'x..x', '.xx.', '....']);
+      assertEqualBoards(['.xx.', 'x..x', '.xx.', '....'], board);
       board = board.play(Weiqi.WHITE, [1, 1]);
-      board.looksLike(['.xx.', 'xo.x', '.xx.', '....']);
+      assertEqualBoards(['.xx.', 'xo.x', '.xx.', '....'], board);
       board = board.play(Weiqi.WHITE, [1, 2]);
-      board.looksLike(['.xx.', 'x..x', '.xx.', '....']);
+      assertEqualBoards(['.xx.', 'x..x', '.xx.', '....'], board);
     });
 
     it('should evaluate enemy liberties before player liberties', function() {
@@ -180,38 +174,38 @@ describe("Board", function() {
                     .play(Weiqi.BLACK, [2, 1])
                     .play(Weiqi.WHITE, [2, 2])
                     .play(Weiqi.BLACK, [1, 0]);
-      board.looksLike(['.xo.', 'x.xo', '.xo.', '....']);
+      assertEqualBoards(['.xo.', 'x.xo', '.xo.', '....'], board);
       board = board.play(Weiqi.WHITE, [1, 1]);
-      board.looksLike(['.xo.', 'xo.o', '.xo.', '....']);
+      assertEqualBoards(['.xo.', 'xo.o', '.xo.', '....'], board);
     });
   });
 
   describe('#areaScore', function() {
     it('should not award points for empty board', function() {
       var board = Weiqi.createBoard(4);
-      board.looksLike(['....', '....', '....', '....']);
+      assertEqualBoards(['....', '....', '....', '....'], board);
       var score = board.areaScore();
-      score[Weiqi.BLACK].should.equal(0);
-      score[Weiqi.WHITE].should.equal(0);
+      assert.equal(0, score[Weiqi.BLACK]);
+      assert.equal(0, score[Weiqi.WHITE]);
     });
 
     it('should award points for board with single stone', function() {
       var board = Weiqi.createBoard(4)
                     .play(Weiqi.BLACK, [0, 0]);
-      board.looksLike(['x...', '....', '....', '....']);
+      assertEqualBoards(['x...', '....', '....', '....'], board);
       var score = board.areaScore();
-      score[Weiqi.BLACK].should.equal(16);
-      score[Weiqi.WHITE].should.equal(0);
+      assert.equal(16, score[Weiqi.BLACK]);
+      assert.equal(0, score[Weiqi.WHITE]);
     });
 
     it('should not award points for sparse board', function() {
       var board = Weiqi.createBoard(4)
                     .play(Weiqi.BLACK, [1, 1])
                     .play(Weiqi.WHITE, [2, 2]);
-      board.looksLike(['....', '.x..', '..o.', '....']);
+      assertEqualBoards(['....', '.x..', '..o.', '....'], board);
       var score = board.areaScore();
-      score[Weiqi.BLACK].should.equal(1);
-      score[Weiqi.WHITE].should.equal(1);
+      assert.equal(1, score[Weiqi.BLACK]);
+      assert.equal(1, score[Weiqi.WHITE]);
     });
 
     it('should count stones and territory', function() {
@@ -224,10 +218,10 @@ describe("Board", function() {
                     .play(Weiqi.WHITE, [1, 2])
                     .play(Weiqi.WHITE, [2, 2])
                     .play(Weiqi.WHITE, [3, 2]);
-      board.looksLike(['.xo.', '.xo.', '.xo.', '.xo.']);
+      assertEqualBoards(['.xo.', '.xo.', '.xo.', '.xo.'], board);
       var score = board.areaScore();
-      score[Weiqi.BLACK].should.equal(8);
-      score[Weiqi.WHITE].should.equal(8);
+      assert.equal(8, score[Weiqi.BLACK]);
+      assert.equal(8, score[Weiqi.WHITE]);
     });
 
     it('should consider all stones alive', function() {
@@ -242,10 +236,10 @@ describe("Board", function() {
                     .play(Weiqi.WHITE, [3, 2])
                     .play(Weiqi.BLACK, [2, 3])
                     .play(Weiqi.BLACK, [3, 3]);
-      board.looksLike(['.xo.', '.xo.', '.xox', '.xox']);
+      assertEqualBoards(['.xo.', '.xo.', '.xox', '.xox'], board);
       var score = board.areaScore();
-      score[Weiqi.BLACK].should.equal(10);
-      score[Weiqi.WHITE].should.equal(4);
+      assert.equal(10, score[Weiqi.BLACK]);
+      assert.equal(4, score[Weiqi.WHITE]);
     });
   });
 });
